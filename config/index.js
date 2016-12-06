53eef5fd58d0ebd9296f045d0eec467e32759602
Hari Menon <imbleedingme@googlemail.com>
2016-12-05 22:53:20 -0500

    update the mongodb uri yet another time

--- config/index.js
+++ config/index.js
@@ -4,7 +4,8 @@ var path = require('path'),
     rootPath = path.normalize(__dirname + '/..'),
     env = process.env.NODE_ENV || 'development',
     user = process.env.MONGO_USER_NAME,
-    pass = process.env.MONGO_USER_PASS;
+    pass = process.env.MONGO_USER_PASS,
+    mongodb_uri = process.env.MONGODB_URI;
 
 var config = {
   development : {
@@ -27,7 +28,7 @@ var config = {
     port  : 1337,
     db    : 'mongodb://localhost/flickr-downloadr-webhook-test'
   },
-  production  : {
+  old_production  : {
     env   : env,
     root  : rootPath,
     mongo : {},
@@ -36,6 +37,16 @@ var config = {
     },
     port  : process.env.PORT,
     db    : 'mongodb://' + user + ':' + pass + '@ds119748.mlab.com:19748/fd-webhook'
+  },
+  production  : {
+    env   : env,
+    root  : rootPath,
+    mongo : {},
+    app   : {
+      name : 'flickr-downloadr-webhook'
+    },
+    port  : process.env.PORT,
+    db    : mongodb_uri
   }
 };
 
