26c6339af2b75468cb9a4f0262cc17c9b46fb938
Floyd Pink <imbleedingme@googlemail.com>
2022-01-15 15:53:17 -0500

    fix deprecation related changes in mongoose 6.x

--- mongoose/index.js
+++ mongoose/index.js
@@ -4,11 +4,6 @@ exports.connect = function () {
   const mongoose = require('mongoose');
   const config = require('../config');
 
-  // reference: https://mongoosejs.com/docs/deprecations.html
-  mongoose.set('useNewUrlParser', true);
-  mongoose.set('useCreateIndex', true);
-  mongoose.set('useUnifiedTopology', true);
-
   mongoose.connect(config.db);
 
   return mongoose.connection;
